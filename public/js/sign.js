04ef54bd85f0919a7cf79d8caea0ce4ad0945d86
Shaun-Shane <1953181@tongji.edu.cn>
2020-11-23 21:53:31 +0800

    upd

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,10 +1,12 @@
 function goBack() {
+  // console.log(window.location.pathname);
+  // while (window.location.pathname == "/public/sign-in.html" || window.location.pathname == "/public/sign-out.html")
     window.history.back();
 }
 
-$("a").click(function () {
-    $("html, body").animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 600);
-    return false;
-});
+// $("a").click(function () {
+//     $("html, body").animate({
+//       scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 600);
+//     return false;
+// });
