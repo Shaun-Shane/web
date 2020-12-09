59d25d04940cb8e6225701257b1ded1c9c31efdd
Shaun-Shane <1953181@tongji.edu.cn>
2020-12-09 21:22:17 +0800

    upd

--- db.js
+++ db.js
@@ -1,23 +1,26 @@
 const ENV = process.env;
-const mysql = require('mysql');
+const sqlite3 = require('sqlite3');
 const dotenv = require('dotenv');
 
 dotenv.config('.env');
 
-const MYSQL_HOST = ENV.MYSQL_HOST || "localhost";
-const MYSQL_DB = ENV.MYSQL_DB;
-const MYSQL_USER = ENV.MYSQL_USER;
-const MYSQL_PASSWORD = ENV.MYSQL_PASSWORD;
 const MYSQL_USER_TABLE = ENV.MYSQL_USER_TABLE;
 
-// console.log(MYSQL_PASSWORD);
+const db = new sqlite3.Database('database.sqlite3', (err) => {
+    if (err) {
+        console.log("Erro opening database " + err.message);
+    } else {
+        db.run(`CREATE TABLE IF NOT EXISTS ${MYSQL_USER_TABLE} (
+        id varchar(255) NOT NULL,
+        username varchar(255) NOT NULL,
+        password varchar(255) NOT NULL,
+        registered datetime CURRENT_TIMESTAMP,
+        last_login datetime CURRENT_TIMESTAMP,
+        PRIMARY KEY (id))`, (err) => {
+            if (err) console.log(err);
+        });
+    }
+})
 
-const connection = mysql.createConnection({
-  host: MYSQL_HOST,
-  user: MYSQL_USER,
-  database: MYSQL_DB,
-  password: MYSQL_PASSWORD
-});
 
-connection.connect();
-module.exports = connection;
+module.exports = db;
--- router.js
+++ router.js
@@ -15,124 +15,123 @@ router.get('/', userMiddleware.isLoggedIn, (req, res) => {
 });
 
 router.get('/sign-up', (req, res) => {
-  res.sendFile(__dirname + '/view/sign-up.html')
+    res.sendFile(__dirname + '/view/sign-up.html')
 });
 
 router.get('/sign-in', (req, res) => {
-  res.sendFile(__dirname + '/view/sign-in.html')
+    res.sendFile(__dirname + '/view/sign-in.html')
 });
 
-router.get('/booking', userMiddleware.isLoggedIn,(req, res) => {
-  //if (typeof req.headers.authorization == "undefined") res.send(emptyFile);
-  res.sendFile(__dirname + '/view/booking.html')
+router.get('/booking', userMiddleware.isLoggedIn, (req, res) => {
+    //if (typeof req.headers.authorization == "undefined") res.send(emptyFile);
+    res.sendFile(__dirname + '/view/booking.html')
 });
 
 router.get('/story', userMiddleware.isLoggedIn, (req, res, next) => {
-  if (req.userData) console.log(req.userData);
-  res.sendFile(__dirname + '/view/story.html')
+    if (req.userData) console.log(req.userData);
+    res.sendFile(__dirname + '/view/story.html')
 });
 
 router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
-  db.query(
-    `SELECT * FROM ${MYSQL_USER_TABLE} WHERE LOWER(username) = LOWER(${db.escape(
-      req.body.username
-    )});`,
-    (err, result) => {
-      if (result.length) {
-        console.log('This username is already in use!');
-        return res.status(409).send({
-          msg: 'This username is already in use!'
-        });
-      } else {
-        // username is available
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).send({
-              msg: err
-            });
-          } else {
-            // has hashed pw => add to database
-            db.query(
-              `INSERT INTO ${MYSQL_USER_TABLE} (id, username, password, registered) VALUES ('${uuid.v4()}', ${db.escape(
-                req.body.username
-              )}, ${db.escape(hash)}, now())`,
-              (err, result) => {
-                if (err) {
-                  throw err;
-                  return res.status(400).send({
-                    msg: err
-                  });
-                }
-                console.log('Registered!');
-                return res.status(201).send({
-                  msg: 'Registered!',
-                  url: "/sign-in"
+    db.get(
+        `SELECT * FROM ${MYSQL_USER_TABLE} WHERE LOWER(username) = LOWER('${req.body.username}');`,
+        (err, result) => {
+            console.log(result);
+            if (result != undefined) {
+                console.log('This username is already in use!');
+                return res.status(409).send({
+                    msg: 'This username is already in use!'
                 });
-              }
-            );
-          }
-        });
-      }
-    }
-  );
+            } else {
+                // username is available
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).send({
+                            msg: err
+                        });
+                    } else {
+                        // has hashed pw => add to database
+                        db.run(
+                            `INSERT INTO ${MYSQL_USER_TABLE} (id, username, password, registered) 
+                            VALUES ('${uuid.v4()}', '${req.body.username}', '${hash}', date('now'))`,
+                            (err, result) => {
+                                if (err) {
+                                    throw err;
+                                    return res.status(400).send({
+                                        msg: err
+                                    });
+                                }
+                                console.log('Registered!');
+                                return res.status(201).send({
+                                    msg: 'Registered!',
+                                    url: "/sign-in"
+                                });
+                            }
+                        );
+                    }
+                });
+            }
+        }
+    );
 });
 
 router.post('/sign-in', (req, res, next) => {
-  db.query(
-    `SELECT * FROM ${MYSQL_USER_TABLE} WHERE username = ${db.escape(req.body.username)};`,
-    (err, result) => {
-      if (err) {
-        throw err;
-        return res.status(400).send({
-          msg: err
-        });
-      }
+    db.get(
+        `SELECT * FROM ${MYSQL_USER_TABLE} WHERE username = '${req.body.username}';`,
+        (err, result) => {
+            if (err) {
+                console.log("err1");
+                throw err;
+                return res.status(400).send({
+                    msg: err
+                });
+            }
 
-      if (!result.length) {
-        return res.status(401).send({
-          msg: "User or password is incorrect!"
-        });
-      }
+            if (result == undefined) {
+                return res.status(401).send({
+                    msg: "User or password is incorrect!"
+                });
+            }
 
-      bcrypt.compare(
-        req.body.password,
-        result[0]['password'],
-        (bErr, bResult) => { //wrong password
-          if (bErr) {
-            throw bErr;
-            return res.status(401).send({
-              msg: 'Username or password is incorrect!'
-            });
-          }
+            bcrypt.compare(
+                req.body.password,
+                result.password,
+                (bErr, bResult) => { //wrong password
+                    if (bErr) {
+                        throw bErr;
+                        return res.status(401).send({
+                            msg: 'Username or password is incorrect!'
+                        });
+                    }
 
-          if (bResult) {
-            const token = jwt.sign({
-                username: result[0].username,
-                userId: result[0].userid
-              },
-              'SECRETKEY', {
-                expiresIn: '7d'
-              }
+                    if (bResult) {
+                        const token = jwt.sign({
+                                username: result.username,
+                                userId: result.id
+                            },
+                            'SECRETKEY', {
+                                expiresIn: '15d'
+                            }
+                        );
+
+                        db.run(
+                            `UPDATE ${MYSQL_USER_TABLE} SET last_login = date('now') WHERE id = '${result.id}'`
+                        );
+
+                        return res.status(200).send({
+                            msg: `Welcome ${result.username}`,
+                            token,
+                            user: result,
+                        });
+                    }
+
+                    return res.status(401).send({ //wrong username or password
+                        msg: 'Username or password is incorrect'
+                    });
+                }
             );
-            
-            db.query(
-              `UPDATE ${MYSQL_USER_TABLE} SET last_login = now() WHERE id = '${result[0].id}'`
-            );
-
-            return res.status(200).send({
-              msg: `Welcome ${result[0].username}`,
-              token,
-              user: result[0],
-            });
-          }
-
-          return res.status(401).send({ //wrong username or password
-            msg: 'Username or password is incorrect'
-          });
         }
-      );
-    }
-  );
+    );
 });
 
 module.exports = router;
